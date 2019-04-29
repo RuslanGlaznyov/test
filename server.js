83c746ee71cc58a2abf7e06d553b0ff442c778a6
RuslanGlaznyov <ruslan.glaznyov@gmail.com>
2019-04-29 16:57:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
+    res.send('Hello fucking world f!');
 });
 
 //asdasd
-app.listen(4000, function () {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
