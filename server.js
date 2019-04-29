f25c449c8c08c8392272eb340ca4e58ef136b6f0
RuslanGlaznyov <ruslan.glaznyov@gmail.com>
2019-04-29 17:15:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
     res.send('Hello fucking world f!');
 });
 
-asdasd
+//asdasd
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
